d63bdc93b42682a5185947b8b5dc64b1983ac61c
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-04-27 15:41:00 +0330

    This class will define the mapping between the customer domain model and the customer DTO.

---
+++ FarzadsTask/Mapping/CustomerMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿
+using AutoMapper;
+using FarzadsTask.Domain;
+using FarzadsTask.Dto;
+
+namespace FarzadsTask.Mapping
+{
+    public class CustomerMappingProfile : Profile
+    {
+        public CustomerMappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>().ReverseMap();
+        }
+    }
+}
