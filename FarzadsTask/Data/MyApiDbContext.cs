c889077e4f9b2ffaa8f3b003ede3fa859a638e5f
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-04-27 15:34:09 +0330

    This class will define the database context and the Customer DbSet.

---
+++ FarzadsTask/Data/MyApiDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FarzadsTask.Domain;
+namespace FarzadsTask.Data
+{
+    public class MyApiDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public MyApiDbContext(DbContextOptions<MyApiDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Customer>().HasKey(e => e.Id);
+            modelBuilder.Entity<Customer>().Property(e => e.FirstName).IsRequired();
+            modelBuilder.Entity<Customer>().Property(e => e.LastName).IsRequired();
+            modelBuilder.Entity<Customer>().Property(e => e.Email).IsRequired();
+        }
+    }
+}
