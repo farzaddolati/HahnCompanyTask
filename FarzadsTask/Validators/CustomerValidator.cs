12c4460d1d65c022f5b8e1aa414c5fdfaeaa1045
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-04-27 15:44:39 +0330

    This class will define the validation rules for the customer DTO.

---
+++ FarzadsTask/Validators/CustomerValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using FarzadsTask.Dto;
+
+namespace FarzadsTask.Validators
+{
+    public class CustomerValidator: AbstractValidator<CustomerDto>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        }
+    }
+}
