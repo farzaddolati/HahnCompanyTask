2de3a61b02424f4b8024210a2638156e12def53b
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-04-27 15:32:02 +0330

    This class will implement the ICustomerRepository interface.

---
+++ FarzadsTask/Data/CustomerRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FarzadsTask.Domain;
+using FarzadsTask.Repositories;
+
+namespace FarzadsTask.Data
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly MyApiDbContext _dbContext;
+
+        public CustomerRepository(MyApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Customer> GetByIdAsync(int id)
+        {
+            return await _dbContext.Customers.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllAsync()
+        {
+            return await _dbContext.Customers.ToListAsync();
+        }
+
+        public async Task AddAsync(Customer customer)
+        {
+            await _dbContext.Customers.AddAsync(customer);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Customer customer)
+        {
+            _dbContext.Entry(customer).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Customer customer)
+        {
+            _dbContext.Customers.Remove(customer);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
