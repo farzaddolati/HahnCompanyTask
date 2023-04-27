0914e7e185893759e4dbe6e30efabbb4178112aa
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-04-27 15:29:57 +0330

    This interface will define the methods for CRUD operations on the customer model.

---
+++ FarzadsTask/Repositories/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FarzadsTask.Domain;
+
+namespace FarzadsTask.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer> GetByIdAsync(int id);
+        Task<IEnumerable<Customer>> GetAllAsync();
+        Task AddAsync(Customer customer);
+        Task UpdateAsync(Customer customer);
+        Task DeleteAsync(Customer customer);
+    }
+}
