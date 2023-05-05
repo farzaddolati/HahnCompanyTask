a09c131c0e2808b87b1af52a55ac1a7eeee93b29
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-05-05 12:48:33 +0330

    Adding the city model and modifying the customer model and adding fields

--- FarzadsTask/Domain/Customer.cs
+++ FarzadsTask/Domain/Customer.cs
@@ -6,5 +6,10 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public int CityId { get; set; }
+        public City City { get; set; }
+
     }
 }
