efe8e08fa9cffbce0d4e3e8478a91d65dabad8dc
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-05-05 13:16:16 +0330

    This interface will define the methods for CRUD operations on the city model

---
+++ FarzadsTask/Data/CityRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FarzadsTask.Domain;
+using FarzadsTask.Repositories;
+namespace FarzadsTask.Data
+{
+    public class CityRepository
+    {
+    }
+}
---
+++ FarzadsTask/Repositories/ICityRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FarzadsTask.Domain;
+
+namespace FarzadsTask.Repositories
+{
+    public interface ICityRepository
+    {
+        Task<City> GetByIdAsync(int id);
+        Task<IEnumerable<City>> GetAllAsync();
+        Task AddAsync(City city);
+        Task UpdateAsync(City city);
+        Task DeleteAsync(City city);
+    }
+}
