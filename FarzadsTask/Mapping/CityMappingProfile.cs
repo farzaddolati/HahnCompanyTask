b60558b48d1ed68f8d3cad302ddb9e3392f45141
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-05-05 13:58:24 +0330

    This class will define the mapping between the city domain model and the city Dto

---
+++ FarzadsTask/Mapping/CityMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿
+using AutoMapper;
+using FarzadsTask.Domain;
+using FarzadsTask.Dto;
+
+namespace FarzadsTask.Mapping
+{
+    public class CityMappingProfile : Profile
+    {
+        public CityMappingProfile()
+        {
+            CreateMap<City, CityDto>().ReverseMap();
+        }
+    }
+}
