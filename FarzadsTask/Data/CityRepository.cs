78f22b09bbd343bbc16252f1ad2d4d5d6f41b09c
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-05-05 13:54:12 +0330

    Add city repository for crud operation

--- FarzadsTask/Data/CityRepository.cs
+++ FarzadsTask/Data/CityRepository.cs
@@ -3,9 +3,44 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using FarzadsTask.Domain;
 using FarzadsTask.Repositories;
+
 namespace FarzadsTask.Data
 {
-    public class CityRepository
+    public class CityRepository : ICityRepository
     {
+        private readonly MyApiDbContext _dbContext;
+
+        public CityRepository(MyApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<City> GetByIdAsync(int id)
+        {
+            return await _dbContext.Cities.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<City>> GetAllAsync()
+        {
+            return await _dbContext.Cities.ToListAsync();
+        }
+
+        public async Task AddAsync(City city)
+        {
+            await _dbContext.Cities.AddAsync(city);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(City city)
+        {
+            _dbContext.Entry(city).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(City city)
+        {
+            _dbContext.Cities.Remove(city);
+            await _dbContext.SaveChangesAsync();
+        }
     }
 }
---
+++ FarzadsTask/Migrations/20230505100818_add_city.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FarzadsTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_city : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CityId",
+                table: "Customers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_CityId",
+                table: "Customers",
+                column: "CityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Cities_CityId",
+                table: "Customers",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Cities_CityId",
+                table: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_CityId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Customers");
+        }
+    }
+}
